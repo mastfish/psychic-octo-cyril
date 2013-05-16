1bea1207f7ca9fcbe33f40a3f9d4f10c2b82f498
Justin Lambert <justin.lambert@terem.com.au>
2013-05-16 14:02:27 +1000

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sharehaus::Application.config.secret_token = '5446184176049dffb6c5b76e0bc148c8fd0dac4abe2772f5196edd2b6506a2ed36036b966f43527fe9655502afd8b2dcc76d7fa145abe6da78d9d5f8e95f8ba7'
